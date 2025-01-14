2292897f1d743ccb74656d8ff8787eee18259e60
Nomaan234 <mdnomaan234@gmail.com>
2025-01-14 23:28:17 +0530

    modulus

---
+++ inc/modulusop.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x=8;
+    int y=x%12;
+    cout<<y;
+
+    // a%b=a ( if a<b)
+    // a%a=0
+    // a%(-b)=a%b
+    // (-a)%b=-[a%b]
+
+}
