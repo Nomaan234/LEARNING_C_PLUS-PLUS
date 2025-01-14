cbd3e8206b18afa4a8cda42b5da4f831f4a931d8
Nomaan234 <mdnomaan234@gmail.com>
2025-01-14 23:53:05 +0530

    ascii value typecasting

--- typecasting.cpp
+++ typecasting.cpp
@@ -2,12 +2,17 @@
 #include <iomanip>
 using namespace std;
 int main()
+// {
+//     int x;
+//     cout<<"enter a number : ";
+//     cin>>x;
+//     cout<<x<<endl;
+//     cout<< (float)x <<endl;
+//     float y = (float)x ; //typecasting
+//     cout<<y;
+// }
 {
-    int x;
-    cout<<"enter a number : ";
-    cin>>x;
-    cout<<x<<endl;
-    cout<< (float)x <<endl;
-    float y = (float)x ; //typecasting
-    cout<<y;
+    char ch;
+    cin>>ch;
+    cout<<(int)ch;
 }
