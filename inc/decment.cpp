c4d56560c7b0c63313ffc22763f5495854f4e559
Nomaan234 <mdnomaan234@gmail.com>
2025-01-14 22:58:56 +0530

    working of pre and post

--- inc/decment.cpp
+++ inc/decment.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 using namespace std;
 int main()
+// {
+//    int x=8;
+//    cout<< x++ <<endl;
+//    cout<< x <<endl;
+//    cout<< (x += 1) <<endl;
+// }
 {
-   int x=8;
-   cout<< x++ <<endl;
-   cout<< x <<endl;
-   cout<< (x += 1) <<endl;
+    int x=8;
+    cout<< x++ <<endl; //post increament (first use the variable then increment it)
+    cout<< x <<endl;
+    cout<< ++x <<endl; //pre increament (first increament the variable then use it)
+    cout<< x <<endl;
+
 }
