51aca0bab33a3f72bd008b8b5a343f744f3c4741
Nomaan234 <mdnomaan234@gmail.com>
2025-01-14 23:43:13 +0530

    tytpecasting

---
+++ typecasting.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+    int x;
+    cout<<"enter a number : ";
+    cin>>x;
+    cout<<x<<endl;
+    cout<< (float)x <<endl;
+    float y = (float)x ; //typecasting
+    cout<<y;
+}
