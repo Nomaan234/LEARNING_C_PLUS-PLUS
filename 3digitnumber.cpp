f9e5f9a920298da396adb3365a6d06a6caef7c82
Nomaan234 <mdnomaan234@gmail.com>
2025-01-15 22:57:41 +0530

    learning conditional

---
+++ 3digitnumber.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter an integer";
+    cin>>n;
+    
+    if(n>99 and n<1000)
+    {
+        cout<<"integer is 3 digit";
+    }
+    else 
+    {
+        cout<<"not a 3 digit integer";
+    }
+}
