c1d4d5ddfb0e426daa29af7267145a4bc3615bfa
Nomaan234 <mdnomaan234@gmail.com>
2025-01-17 22:35:27 +0530

    pattern printing

---
+++ pattrenprinting.cpp/numbertriangle.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n,m;
+    cout<<"enter number of row : ";
+    cin>>n;
+    // cout<<"enter number of column : ";
+    // cin>>m;
+
+    for(int i=1 ; i<=n ; i++)
+    {
+        for(int j=1 ; j<=i ; j++)
+        {
+            cout<<j;
+        }
+        cout<<endl;
+    }
+
+}
